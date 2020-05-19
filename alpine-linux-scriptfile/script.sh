d6f5e7c3b43be6372b91c6106f7f1df2fc722c02
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-19 07:23:50 +0530

    added alpine linux script file work

---
+++ alpine-linux-scriptfile/script.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# https://forums.docker.com/t/getting-panic-spanic-standard-init-linux-go-178-exec-user-process-caused-no-such-file-or-directory-red-while-running-the-docker-image/27318/4
+# https://stackoverflow.com/questions/27810758/how-to-replace-crlf-with-lf-in-a-single-file
+
+echo Hello, from a script file!
