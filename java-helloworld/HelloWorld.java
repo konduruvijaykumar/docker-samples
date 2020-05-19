e9a13936c3441f943cdb814516fb8609fccb26b2
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-19 10:50:15 +0530

    added java hello world docker

---
+++ java-helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author Vijay
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Java Application! Welcome to Docker World");
+	}
+
+}
