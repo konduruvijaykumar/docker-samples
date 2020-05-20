7a99f936ff76178d29f5a9d75688ee69e2e2c3e3
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-20 10:22:25 +0530

    adding student info RESTful project

---
+++ springboot-studentinfo/src/main/java/org/pjay/student/SpringbootStudentinfoApplication.java
@@ -0,0 +1,13 @@
+package org.pjay.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentinfoApplication.class, args);
+	}
+
+}
