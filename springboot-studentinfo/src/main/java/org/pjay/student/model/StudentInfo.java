0b9cb42b417745a129ad6fe314947da1af556dbf
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-21 22:05:43 +0530

    Initial commit

---
+++ springboot-studentinfo/src/main/java/org/pjay/student/model/StudentInfo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.pjay.student.model;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author vijayk
+ *
+ */
+@Component
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StudentInfo {
+	
+	
+
+}
