f06ecc4d57a50b0c2343f19dc803a1d8dcbe410b
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-22 06:19:06 +0530

    service implementation skeleton added

---
+++ springboot-studentinfo/src/main/java/org/pjay/student/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package org.pjay.student.serviceimpl;
+
+import java.util.List;
+
+import org.pjay.student.entity.Student;
+import org.pjay.student.service.StudentService;
+
+/**
+ * @author vijayk
+ *
+ */
+public class StudentServiceImpl implements StudentService{
+
+	@Override
+	public Student getStudentById(Long studentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student createStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteStudent(Student student) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteStudentById(Long studentId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
