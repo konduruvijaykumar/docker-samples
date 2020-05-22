a8ed426c8b75e596546ba802f7f0585598007616
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-22 06:34:23 +0530

    controller advice skeleton added

---
+++ springboot-studentinfo/src/main/java/org/pjay/student/controller/StudentApplicationControllerAdvice.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.student.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author vijayk
+ *
+ */
+@RestControllerAdvice
+public class StudentApplicationControllerAdvice {
+	
+	
+
+}
