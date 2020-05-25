f25940b943906433acf6b02b991296f6da3f79fc
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-25 22:00:57 +0530

    added skeleton of project

---
+++ springboot-mvc-studentinfo/src/main/java/org/pjay/student/SpringbootMvcStudentinfoApplication.java
@@ -0,0 +1,13 @@
+package org.pjay.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcStudentinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcStudentinfoApplication.class, args);
+	}
+
+}
---
+++ springboot-mvc-studentinfo/src/test/java/org/pjay/student/SpringbootMvcStudentinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pjay.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcStudentinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
