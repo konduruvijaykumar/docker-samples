a7f27cbdc151033a3dab3f7fe5ccbfbae831f1d7
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-26 09:24:19 +0530

    corrections to previous commit

--- springboot-mvc-studentinfo/src/main/java/org/pjay/student/SpringbootMvcStudentinfoApplication.java
+++ springboot-mvc-studentinfo/src/main/java/org/pjay/student/SpringbootMvcStudentinfoApplication.java
@@ -2,12 +2,19 @@ package org.pjay.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootMvcStudentinfoApplication {
+public class SpringbootMvcStudentinfoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMvcStudentinfoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootMvcStudentinfoApplication.class);
 	}
 
 }
